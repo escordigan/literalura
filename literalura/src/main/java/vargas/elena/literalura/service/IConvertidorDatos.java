3f196cf3049c1af5857b08724fb1c4aaaf9eaf3b
Elena Vargas <149540525+escordigan@users.noreply.github.com>
2024-06-06 15:16:08 -0600

    Update IConvertidorDatos.java

--- literalura/src/main/java/vargas/elena/literalura/service/IConvertidorDatos.java
+++ literalura/src/main/java/vargas/elena/literalura/service/IConvertidorDatos.java
@@ -2,8 +2,6 @@ package vargas.elena.literalura.service;
 
 public interface IConvertidorDatos {
 
-    //La interface contiene el metodo generico para obtener los datos de API//
-
     <T> T obtenerDatos(String json, Class<T> clase);
 
 }
